73850f8a20340f40f3fdcdf7601109fc0706812a
Hossein Shakouri <mhshakouri@gmail.com>
2023-05-25 22:16:26 +0300

    changed sample data

--- src/scripts/mockdata.ts
+++ src/scripts/mockdata.ts
@@ -1,4 +1,4 @@
-type Product = {
+export type Product = {
     id: string
     name: string
     price: string
@@ -6,33 +6,33 @@ type Product = {
     link: string
 }
 
-const products: Product[] = [
+export const products: Product[] = [
 
     {
         id: "1",
-        name: "clip x1",
-        price: "9.99",
+        name: "clip x3",
+        price: "€ 12.99",
         img: "https://tractive.com/assets/image/shop-frontend/product/trnjaca/tractive-gps-dog-4-spare-charger.png",
         link: "#"
     },
     {
         id: "2",
-        name: "clip x2",
-        price: "9.99",
+        name: "clip XL x3",
+        price: "€ 19.99",
         img: "https://tractive.com/assets/image/shop-frontend/product/trnjaca/tractive-gps-dog-4-spare-charger.png",
         link: "#"
     },
     {
         id: "3",
-        name: "clip x3",
-        price: "9.99",
+        name: "Spare Chargin Cable",
+        price: "€ 9.99",
         img: "https://tractive.com/assets/image/shop-frontend/product/trnjaca/tractive-gps-dog-4-spare-charger.png",
         link: "#"
     },
     {
         id: "4",
-        name: "clip x4",
-        price: "9.99",
+        name: "x3 Spare Charging Cable",
+        price: "€ 15.99",
         img: "https://tractive.com/assets/image/shop-frontend/product/trnjaca/tractive-gps-dog-4-spare-charger.png",
         link: "#"
     }
