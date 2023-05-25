ac27edee6d9104e792132e020da855fa62dd245b
Hossein Shakouri <mhshakouri@gmail.com>
2023-05-25 20:08:18 +0300

    replace mock json data with an object for easier use

---
+++ src/scripts/mockdata.ts
@@ -0,0 +1,39 @@
+type Product = {
+    id: string
+    name: string
+    price: string
+    img: string
+    link: string
+}
+
+const products: Product[] = [
+
+    {
+        id: "1",
+        name: "clip x1",
+        price: "9.99",
+        img: "https://tractive.com/assets/image/shop-frontend/product/trnjaca/tractive-gps-dog-4-spare-charger.png",
+        link: "#"
+    },
+    {
+        id: "2",
+        name: "clip x2",
+        price: "9.99",
+        img: "https://tractive.com/assets/image/shop-frontend/product/trnjaca/tractive-gps-dog-4-spare-charger.png",
+        link: "#"
+    },
+    {
+        id: "3",
+        name: "clip x3",
+        price: "9.99",
+        img: "https://tractive.com/assets/image/shop-frontend/product/trnjaca/tractive-gps-dog-4-spare-charger.png",
+        link: "#"
+    },
+    {
+        id: "4",
+        name: "clip x4",
+        price: "9.99",
+        img: "https://tractive.com/assets/image/shop-frontend/product/trnjaca/tractive-gps-dog-4-spare-charger.png",
+        link: "#"
+    }
+]
